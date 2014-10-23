7ad95df182d5b2ae9368aefcce155705223b9c19
Roger Tanuatmadja <rtanuatm@eng.ucsd.edu>
2014-10-22 17:05:11 -0700

    Added Spring's application context.

--- src/main/java/edu/ucsd/xmlparser/ParserMain.java
+++ src/main/java/edu/ucsd/xmlparser/ParserMain.java
@@ -2,10 +2,15 @@ package edu.ucsd.xmlparser;
 
 import java.io.File;
 
+import org.springframework.context.ApplicationContext;
+
+import edu.ucsd.system.SystemApplicationContext;
+
 public class ParserMain {
 
 	public static void main(String[] args) throws Exception {
 		File financialFile = new File(ParserMain.class.getClassLoader().getResource("sample-financial.xml").getFile());
+		ApplicationContext context = SystemApplicationContext.getApplicationContext();
 		FinancialXMLParser parser = new FinancialXMLParser();
 		parser.parse(financialFile);
 	}

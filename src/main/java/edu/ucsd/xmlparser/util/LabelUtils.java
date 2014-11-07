1905321182a4946dd77e164fd1b34d651885efae
Roger Tanuatmadja <rtanuatm@eng.ucsd.edu>
2014-11-07 12:14:29 -0800

    Added new entities and utilities.

---
+++ src/main/java/edu/ucsd/xmlparser/util/LabelUtils.java
@@ -0,0 +1,17 @@
+package edu.ucsd.xmlparser.util;
+
+public abstract class LabelUtils {
+	private static String LABELSTRATEGY_PREFIX = "_";
+	
+	public static String createLabel(String userSuppliedLabel) {
+		return LABELSTRATEGY_PREFIX + userSuppliedLabel;
+	}
+	
+	public static String labelPOS(String posInformation) {
+		return "POS{" + posInformation + "}";
+	}
+	
+	public static String labelNE(String neInformation) {
+		return "NE{" + neInformation + "}";
+	}
+}

627a7fbe18983a2b05735774f6a6242e7b4cd235
Roger Tanuatmadja <rtanuatm@eng.ucsd.edu>
2015-01-25 11:58:23 -0800

    Added missing file

--- src/main/java/edu/ucsd/query/QueryManager.java
+++ src/main/java/edu/ucsd/query/QueryManager.java
@@ -1,10 +1,12 @@
 package edu.ucsd.query;
 
+import edu.ucsd.grammar.ParsedQuery;
 import edu.ucsd.system.SystemApplicationContext;
 
 public class QueryManager {
 	public QueryResult executeQuery(ParsedQuery parsedQuery) {
 		QueryExecutor queryExecutor = (QueryExecutor)SystemApplicationContext.getApplicationContext().getBean("queryExecutor");
+		
 		return null;
 	}
 }

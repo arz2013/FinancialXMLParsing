fb2231e1eae5722811ca563f29f1b134589bbf20
Roger Tanuatmadja <rtanuatm@eng.ucsd.edu>
2015-01-25 19:02:23 -0800

    Refactoring

--- src/test/java/edu/ucsd/query/QueryManagerTest.java
+++ src/test/java/edu/ucsd/query/QueryManagerTest.java
@@ -1,5 +1,6 @@
 package edu.ucsd.query;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,6 +16,7 @@ public class QueryManagerTest {
 	
 	@Test
 	public void testFindShortestTerm() throws ParseException {
-		queryManager.executeQuery(Query.createQuery("for w:Word, p = shortest_term_starting_with(w) where w = 'Walt' return p").getParsedQuery());
+		QueryResult<String> result = queryManager.executeQuery(Query.createQuery("for w:Word, p = shortest_term_starting_with(w) where w = 'Walt' return p").getParsedQuery(), String.class);
+		Assert.assertEquals(result.getResult(), "Walt Disney");
 	}
 }

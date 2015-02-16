254940f4c4f6d4b30f3357561fab7757c67171ee
Roger Tanuatmadja <rtanuatm@eng.ucsd.edu>
2015-02-16 14:25:09 -0800

    Temporary check in.

--- src/main/java/edu/ucsd/xmlparser/ParserMain.java
+++ src/main/java/edu/ucsd/xmlparser/ParserMain.java
@@ -6,21 +6,26 @@ import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 
+import edu.ucsd.cvalue.CValueDocumentCalculator;
 import edu.ucsd.system.SystemApplicationContext;
 
 public class ParserMain {
 
 	public static void main(String[] args) throws Exception {
+		ApplicationContext context = SystemApplicationContext.getApplicationContext();
+
 		String[] fileNames = { "ShortenedTestDocument.xml", "ShortenedTestDocument2.xml" };
 		List<File> files = new ArrayList<File>();
 		for(String fileName : fileNames) {
 			files.add(new File(ParserMain.class.getClassLoader().getResource(fileName).getFile()));
 		}
-		ApplicationContext context = SystemApplicationContext.getApplicationContext();
 		FinancialXMLParser parser = FinancialXMLParser.class.cast(context.getBean("financialXMLParser"));
 		for(File file : files) {
 			parser.parseAndLoad(file, 2013);
 		}
+
+		CValueDocumentCalculator cValueDocumentCalculator = CValueDocumentCalculator.class.cast(context.getBean("cValueDocumentCalculator"));
+		cValueDocumentCalculator.computeCollectionLevelCValue();
 	}
 
 }

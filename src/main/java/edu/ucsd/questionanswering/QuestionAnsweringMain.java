d649ce2cc5a902c9272a6d2bd41e482ddc9a55d4
Roger Tanuatmadja <rtanuatm@eng.ucsd.edu>
2015-03-01 11:26:37 -0800

    Added logging capabilities

--- src/main/java/edu/ucsd/questionanswering/QuestionAnsweringMain.java
+++ src/main/java/edu/ucsd/questionanswering/QuestionAnsweringMain.java
@@ -1,15 +1,18 @@
 package edu.ucsd.questionanswering;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 
 import edu.ucsd.system.SystemApplicationContext;
 
 public class QuestionAnsweringMain {
-
+	private static Logger logger = LoggerFactory.getLogger(QuestionAnsweringMain.class);
+	
 	public static void main(String[] args) {
 		ApplicationContext ctx = SystemApplicationContext.getApplicationContext();
 		QuestionAnsweringModule qaModule = QuestionAnsweringModule.class.cast(ctx.getBean("questionAnsweringModule"));
-		System.out.println(qaModule.answer("Which organizations did Chevron acquire?"));
+		logger.info(qaModule.answer("Which organizations did Chevron acquire?").toString());
 		/**
 		qaModule.answer("Which organizations did Walt Disney acquire?");
 		qaModule.answer("What acquisitions did Chevron make?"); // Unable to answer this one

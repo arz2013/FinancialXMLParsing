d5c324ffcb35cb2081812f276bc61dd39755f132
Roger Tanuatmadja <rtanuatm@eng.ucsd.edu>
2015-03-18 10:42:34 -0700

    New Relationship type

--- src/main/java/edu/ucsd/xmlparser/entity/ApplicationRelationshipType.java
+++ src/main/java/edu/ucsd/xmlparser/entity/ApplicationRelationshipType.java
@@ -14,5 +14,7 @@ public enum ApplicationRelationshipType implements RelationshipType {
 	NEXT_WORD, 
 	REFERS_TO, 
 	// Constructs the relationship between Document and #document
-	RELATED_DOCUMENT;
+	RELATED_DOCUMENT, 
+	// Constructs the relationship between Collection and Document
+	HAS_DOCUMENT;
 }
